2a23f84755f48efc97a3ef3e1fd43e28f70b8828
Arif Bishwas <60783337+arifbiswas@users.noreply.github.com>
2022-10-25 02:15:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors);
+
+const port = process.env.PROT || 5000
+
+app.get('/',(req,res)=>{
+    res.send('Is ti running');
+})
+
+app.listen(port,()=>{
+    console.log('this is site run are',5000);
+})
