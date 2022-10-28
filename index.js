f7a2d1d3726d8f0b5e29f3f718f5b7029046031f
Arif Bishwas <60783337+arifbiswas@users.noreply.github.com>
2022-10-28 13:55:38 +0600

    add catrgory json file

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ const cors = require('cors');
 const app = express()
 app.use(cors());
 
+const courses = require('./data/courses.json')
+const category = require('./data/category.json')
+
 app.get('/',(req,res)=>{
     res.send('this server is running ')
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+app.get('/courses/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const selectedCourses = courses.find(c => c.courses_id == id) || {};
+
+    res.send(selectedCourses)
+})
+app.get('/category',(req,res)=>{
+    res.send(category)
+})
+app.get('/category/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    
+    const selectedCourses = courses.find(c => c._id == id) || courses;
+
+    res.send(selectedCourses)
 })
 
 app.listen(port, () => {
